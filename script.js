3b0528adc66327cc65c73c9529a61328d4837552
AlexXXX <lyoshkaslip777@yandex.ru>
2019-11-28 13:47:08 +0300

    это наш первый комит

---
+++ script.js
@@ -0,0 +1,748 @@
+"use strict";
+
+
+var number = 5;
+var string = "hello";
+var sym = Symbol() ;
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log (persone.name);
+console.log (persone["name"]);
+
+let arr = ["plum.png",'orange.jpg','apple.bmp']
+;
+console.log (arr[0]);
+
+
+
+
+
+
+// let answer = +prompt("Est' li vam 18?", "da");
+// console.log(answer);
+
+// console.log (typeof(answer));
+
+// console.log ("arr"+" - object");
+// console.log (4+ +" - object");
+let incr = 10,
+decr = 10;
+// incr++
+// decr--;
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log ("2"===2);
+let isChecked = true,
+isClose = false;
+// console.log(isChecked && isClose)
+console.log(isChecked || isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+
+ 
+// let greeting = function ( name = "John Doe", day ) {
+//     console.log('Privet ' + name + '! Have a good day'); 
+//     if (day == 'subbota' || day == 'voskresenye'); {
+//         console.log("Have a happy holidays!")
+//     } 
+//     if (day == 'ponedelnik' || 'vtornik' || 'sreda' || 'chetverg' || 'pyatnica'){
+//         console.log("happy working!");
+//     }
+// }
+
+// greeting("Вася", 'voskresenye');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //   переменные
+
+
+
+
+
+
+
+
+
+// var  LeftBorderWidth = 1;
+// let second =2;
+// const pi = 3.14;
+
+// console.log (LeftBorderWidth);
+// console.log (second); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 5;
+// let y = 10;
+// let sum = x + y;
+// console.log (sum) ; // в консоль выдаётся сумма (15)
+// let x = 100;
+// let y = 200;
+// let sum = x + y;
+// console.log (sum);
+
+// let x = 300;
+// let y = 500;
+// let sum = x + y;
+// console.log (sum);
+
+// let summa = function () {
+//     console.log (" privet! have nice day");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let countries = [
+//     "Russia",
+//     "USA",
+//     "Italy",
+//     "France",
+// ];
+
+// console.log (countries.length);
+
+
+
+
+// let i 
+//  for (let i =0; i<10; i++){
+//      console.log(i);
+//  }
+// console.log (i);
+
+
+
+
+
+
+
+
+// let i;
+// for (начальное значение; условие; действие после цикла;) {}
+// // код цикла
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let country1 = "Russia";
+// let country2 = "USA";
+// let country3 = "Italy";
+// let country4 = "France";
+
+// console.log (country1);
+// console.log (country2);
+// console.log (country3);
+// console.log (country4);
+
+// let countries = [
+//     "Russia",
+//     "USA",
+//      "Italy",
+//     "France"
+// ];
+
+// console.log ('выводим элементы массива');
+// console.log (countries [0] );
+// console.log (countries [1]);
+// console.log (countries [2]);
+// console.log (countries [3]);
+
+
+// console.log ("выводим элементы массива через метод forEach");
+// countries.forEach (function(entry) {
+//     console.log(entry);
+// } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 5;
+
+// switch (a){
+//     case 0:
+//         console.log ("a ravno 0");
+//         break;
+//         case 5: 
+//         console.log ("a ravno 5");
+//         break;
+//         case 10:
+//         console.log ("a ravno 10");
+//         break;
+//         default:
+//             console.log ("strannoe znachenie");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = 15;
+// if (a == 0) {
+//     console.log (" a ravno 0");
+// }
+// else if ( a == 5){
+//     console.log ("a ravno 5");
+// }
+// else if ( a == 10){
+//     console.log ("a ravno 10");
+// }
+// else if ( a == 15){
+//     console.log ("a ravno 15");
+// }
+// else {
+//     console.log ("strannoe znachenie");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = 5;
+
+// if (age <18 || age > 60) {
+//     console.log (" Vam menshe 18 ili bolshe 60");
+// }
+
+//   && - оператор "и"
+//   || - оператор или
+
+
+
+
+// let a = 5;
+// if (a > 10) {
+//     console.log ("a bolshe 10");
+
+// }
+// else if ( a == 10) {
+//     console.log ("a ravno 10"); 
+// }
+// else {
+//     console.log ("a menshe 10");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = true;
+// let b = false;
+// // let a = "Hello ";
+// // let b = "world!";
+// // let result = a + b;
+// // console.log (result);
+// console.log (a + b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 5;
+// let y = 20;
+// let result;
+// result = x / y;
+// console.log (result);
+
+
+/*
+++i - сразу увеличивает переменную на 1
+i++ - следующим шагом увеличивает переменную на 1
+--i
+i++ 
+*/
+
+
+// let i = 10;
+// console.log (i--);
+// console.log (i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let x = 5;    // Integer (числа)
+// let text = "строка"; //(текст)
+// let logic1 = true; // (правда, 1)
+// let logic2 = false; // (ложь, 0)
+
+
+// console.log (x);
+// console.log (text);
+// console.log (logic1);
+// console.log (logic2);
+
+
+
+
+
+
+
+
+
+
+// let age =20;
+// if (age < 18){
+//     console.log  ("щенок ещё");
+    
+// }
+// else {
+//     console.log  (" какой большой ");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let name = "Ivan";
+// alert(`hello, ${name}`); 
+// alert(`rezultat: ${10-5}`);  
+// console.log (" balbes ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let admin = "Джон";
+    // let name = admin;;
+    // alert(admin);  
+    // let Earth = "Земля";
+    // let currentUserName = "Alex";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let username = "alex";
+// let firstname = "gryzlov";
+// alert(username);
+// alert(firstname);
+// const myBirthday = "09.10.1991";
+// alert(myBirthday);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let message;
+// let hello;
+// hello = message;
+// message = "hello";
+// hello = "hello";
+// alert(hello);
+// alert(message);
+// let world;
+// world = "my darling";
+// alert(world);
+// const myBirthday = "09.10.1991";
+// alert(myBirthday); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert ("luckystar");
+
+// let green = prompt ("введите свой возраст");
+// if (green < 18) {
+//     console.log ("Слишком мал") ;
+// }
+// else {
+// console.log ("Достаточно взрослый") } ; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let vasya = prompt("Введите свой возраст", 0); 
+
+// if (vasya <= 18) {
+//     console.log("Privet Vasya");
+// } else {
+//     console.log("Privet pensioner") ;
+// 
